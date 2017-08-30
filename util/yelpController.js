112181b4db57bd0f32c5d3d8f2577570c95e443d
Jon Roach <roach.jc@gmail.com>
2017-08-29 17:00:46 -0700

    beginning work on yelp api

---
+++ util/yelpController.js
@@ -0,0 +1,6 @@
+const rp = require('request-promise-native');
+
+const client_id = '2tNV98xeiZ93EqTcUR4Vvw';
+const client_secret = 'mK31beLrNq7QkEcMf4Q3Izl83cj9ILNqSPBnrha6GoGCSHr86Eh135hlC162bceE';
+const token = '6i0pYlDOl4BDG3lS0pIKSZu96vUhx0NpPyFH6EKDVDrqTc1PNHxQUBz8PlIpo6xyVWyPMnkxaX9yh07IVwXx4wt5P6Iv8uzDdkxTnSNo9bzvt0Aseljt2DSMhe2lWXYx';
+
