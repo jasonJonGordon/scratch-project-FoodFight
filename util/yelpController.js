eb8ca4d3206f5b9543fd30a0911670ded377acb8
Jon Roach <roach.jc@gmail.com>
2017-08-29 17:02:37 -0700

    yelp work

--- util/yelpController.js
+++ util/yelpController.js
@@ -1,6 +1,16 @@
 const rp = require('request-promise-native');
 
-const client_id = '2tNV98xeiZ93EqTcUR4Vvw';
-const client_secret = 'mK31beLrNq7QkEcMf4Q3Izl83cj9ILNqSPBnrha6GoGCSHr86Eh135hlC162bceE';
 const token = '6i0pYlDOl4BDG3lS0pIKSZu96vUhx0NpPyFH6EKDVDrqTc1PNHxQUBz8PlIpo6xyVWyPMnkxaX9yh07IVwXx4wt5P6Iv8uzDdkxTnSNo9bzvt0Aseljt2DSMhe2lWXYx';
 
+const yelpController = {
+  getData(term, location = '90292') {
+    const options = {
+      uri: `https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}`,
+      headers: {
+        Authorization: 
+      }
+    }
+  },
+}
+
+module.exports = yelpController;
